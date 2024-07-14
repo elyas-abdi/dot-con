38576d5810238cbe68165c0d5678fa9881f30c3c
elyas <savior@savior.software>
2024-07-14 14:22:36 -0700

    feat: add slice accessor

--- access.go
+++ access.go
@@ -1,5 +1,40 @@
 package config
 
+import `strings`
+
+func (c *Config) String(key string) *accessStringOption {
+	return &accessStringOption{
+		accessOption{
+			key:       key,
+			c:         c,
+			accessArg: make(map[string]string),
+		},
+	}
+}
+
 func (o *accessStringOption) Access() *string {
 	return o.c.access(o.key)
+}
+
+func (c *Config) Slice(key string) *accessStringSliceOption {
+	return &accessStringSliceOption{
+		accessOption{
+			key:       key,
+			c:         c,
+			accessArg: make(map[string]string),
+		},
+	}
+}
+
+func (o *accessStringSliceOption) Access() *[]string {
+	val := o.c.access(o.key)
+	if val == nil {
+		return nil
+	}
+
+	sanitized := *val
+	sanitized = sanitized[1 : len(sanitized)-1]
+	slice := strings.Split(sanitized, ", ")
+
+	return &slice
 }
--- option.go
+++ option.go
@@ -1,7 +1,5 @@
 package config
 
-import `strings`
-
 type accessOption struct {
 	key       string
 	accessArg map[string]string
@@ -9,18 +7,4 @@ type accessOption struct {
 }
 
 type accessStringOption struct{ accessOption }
-
-func (o *accessStringOption) Arg(factor, definition string) *accessStringOption {
-	o.accessArg[strings.ToUpper(factor)] = strings.ToUpper(definition)
-	return o
-}
-
-func (c *Config) String(key string) *accessStringOption {
-	return &accessStringOption{
-		accessOption{
-			key:       key,
-			c:         c,
-			accessArg: make(map[string]string),
-		},
-	}
-}
+type accessStringSliceOption struct{ accessOption }
